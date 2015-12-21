38d9b0d616bc5db5781ba7bebd881b57301822f5
yufu <vzheng56@gmail.com>
2015-12-21 11:11:23 +0800

    Unity Demo
    Add an Unity Demo!!!

---
+++ QRCodeUnityDemo/Assets/Demo/DemoUsage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+using QRCoder;
+
+public class DemoUsage : MonoBehaviour {
+
+	public RawImage textureUI; 
+	public InputField fieldText;
+	private string fieldTextContent;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void QRGet(){
+		QRCodeGenerator qrGenerator = new QRCodeGenerator();
+		QRCodeData qrCodeData = qrGenerator.CreateQrCode(fieldTextContent, QRCodeGenerator.ECCLevel.Q);
+		UnityQRCode qrCode = new UnityQRCode(qrCodeData);
+		Texture2D qrCodeImage = qrCode.GetGraphic(20);
+		textureUI.texture = qrCodeImage;
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if (fieldText != null) {
+			fieldTextContent = fieldText.text;
+		}
+	}
+}
